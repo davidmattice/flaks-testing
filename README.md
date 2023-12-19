fbc93e6ecde07dddd5964edd4af48e5a1bed6a1e
Dave Mattice <david.mattice@hotmail.com>
2023-12-19 12:55:45 -0500

    checkpoint

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Super ddetailed instructions
+1. Start vscode in this folder with - code .
+2. Create an environment - <ctrl><shift>P
+3. Install Flash - python3 -m pip install flask
+4. Run app - python3 -m flask run
+
--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 msg = "Hello World2"
+enabled = 0
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html', msg=msg)
+    global enabled
+    activate = None
+
+    if request.method == 'POST':
+        activate = request.form.get('activate')
+        if activate == "enable":
+            enabled = 1
+        elif activate == "disable":
+            enabled = 0
+
+    return render_template('index.html', msg=msg, enabled=enabled)
 
 if __name__ == "app":
     msg="Bob"
